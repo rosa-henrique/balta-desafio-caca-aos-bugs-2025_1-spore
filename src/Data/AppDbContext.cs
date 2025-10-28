30e3e43768ad172b7a886b1be42a8e2f6a62bcba
Henrique <rosa.henrique@outlook.com.br>
2025-10-28 19:40:05 -0300

    finalizado desafio

--- src/Data/AppDbContext.cs
+++ src/Data/AppDbContext.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace BugStore.Data;
 
-public class AppDbContext : DbContext
+public class AppDbContext(IConfiguration configuration) : DbContext
 {
     public DbSet<Customer> Customers { get; set; } = null!;
     public DbSet<Product> Products { get; set; } = null!;
@@ -11,5 +11,5 @@ public class AppDbContext : DbContext
     public DbSet<OrderLine> OrderLines { get; set; } = null!;
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseSqlite();
+        => optionsBuilder.UseSqlite(configuration.GetConnectionString("DefaultConnection"));
 }
